9b485a0af6cf10cf52d3b8fdf958f4e636f072ae
Edward Angelo Salas <zyro670@gmail.com>
2022-03-20 17:42:49 -0400

    commit tree 9328da703eefd98a26fa1d2f7ecf91af99084c5f

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(msg) {
+    if (msg != null)
+        return 'This Saturday, I want to bathe my dog!';
+    else
+        return 'This Saturday, I want to roller-skate!';
+}
+function mondayWork(msg) {
+    if (msg != null)
+        return 'This Monday, I will work from home.';
+    else
+        return 'This Monday, I will go to the office.';
+}
+function wrapAdjective() {
+    function wrapAdjective2() {
+        var msg = 'You are';
+        wrapAdjective2("*");
+        var emphatic = wrapAdjective2("a hard worker");
+        msg = msg + emphatic;
+        return msg;
+    }
+}
